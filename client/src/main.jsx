7d07b9d66f2447c879908377a69e0c4389b403c2
Augustine Namare <agNamare@gmail.com>
2023-10-25 10:24:36 +0300

    add redux persist

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,12 +3,15 @@ import App from './App.jsx';
 import React from 'react';
 import './index.css';
 import { Provider } from 'react-redux';
-import { store } from './redux/store.js';
+import { persistor, store } from './redux/store.js';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,23 @@
-import {configureStore, getDefaultMiddleware} from "@reduxjs/toolkit";
+import {combineReducers, configureStore, getDefaultMiddleware} from "@reduxjs/toolkit";
 import userReducer from "./user/userSlice";
+import {persistReducer, persistStore} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const rootReducer = combineReducers({user:userReducer});
+
+const persistConfig = {
+  key: "root",
+  storage, 
+  version:1
+}
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
-  reducer:{user: userReducer},
+  reducer: persistedReducer,
   // Disable serializability check for Redux state updates.
   middleware: (getDefaultMiddleware)=> getDefaultMiddleware({
     serializableCheck:false
   }),
 });
+
+export const persistor = persistStore(store);
