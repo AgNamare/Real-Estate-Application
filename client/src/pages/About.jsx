b6f1112e7173c1db4fb6a84b2243353ba4b3111a
Augustine Namare <agNamare@gmail.com>
2024-02-01 15:29:50 +0300

    Complete About Page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-
 export default function About() {
   return (
-    <div>
-      Home
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800
+      ">About Edge Estate</h1>
+      <p className="mb-4 text slate-800">Discover a new standard of elegance and sophistication at Edge Estate, where modern design seamlessly blends with the tranquility of nature. Our visionary real estate project is a testament to the art of fine living, offering a unique combination of opulence, comfort, and sustainability.</p>
     </div>
   )
 }
