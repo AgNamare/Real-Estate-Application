7845e75d1221e77aac6beae23856cf8dc5fe902b
Augustine Namare <agNamare@gmail.com>
2024-04-13 12:12:26 +0300

    Fix syntax error

--- api/index.js
+++ api/index.js
@@ -27,7 +27,7 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-app.use(express.static(path.join(__dirname, '/client.dist')));
+app.use(express.static(path.join(__dirname, '/client/dist')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, "client", "dist", "index.html"))
