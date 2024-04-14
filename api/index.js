79211e47b320a3a82adbca8dac025ba5766d1bdc
Augustine Namare <agNamare@gmail.com>
2024-04-14 05:26:41 +0300

    Fix CORS

--- api/index.js
+++ api/index.js
@@ -7,17 +7,18 @@ import listingRouter from "./routes/listing.route.js";
 import authRouter from "./routes/auth.route.js";
 import bodyParser from "body-parser";
 import cors from "cors";
-import path from "path";
+import path from "path"
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGOURL).then(() => {
+mongoose.connect(process.env.MONGOURL).then(()=>{
   console.log("Connected to MongoDB");
-}).catch(err => {
+}).catch(err=>{
   console.log("Error: ", err);
 });
 
 const __dirname = path.resolve();
+
 
 const app = express();
 app.use(express.json());
@@ -27,32 +28,34 @@ app.use(bodyParser.urlencoded({
 }));
 
 const corsOptions = {
-  origin: ['http://localhost:5173', 'https://edge-estate.onrender.com'],
+  origin: ['http://localhost:5173', 'https://edge-estate.onrender.com'], 
   credentials: true
 };
 
 app.use(cors(corsOptions));
 
+
 app.listen(5000, function () {
   console.log('Server is running on port 5000!');
-});
-
+ });
+ 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
 
+
 app.use(express.static(path.join(__dirname, '/client/dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
-});
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"))
+})
 
-app.use((err, req, res, next) => {
+ app.use((err, req, res, next)=>{
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
   return res.status(statusCode).json({
     success: false,
-    statusCode,
+    statusCode, 
     message
   });
-});
+ });
